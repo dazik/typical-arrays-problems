162285501b6af6b02bd346199c75b227a9cbd594
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-10-01 20:23:20 +0500

    init: add three functions to solve the typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
-exports.min = function min (array) {
-  return 0;
+function isEmpty(array) {
+    if (array === undefined || array.length === 0) return true;
+    else return false;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function min(array) {
+    if (isEmpty(array)) return 0;
+    let min = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+
+        }
+    }
+    return min;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.max = function max(array) {
+    if (isEmpty(array)) return 0;
+    let max = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+exports.avg = function avg(array) {
+    let sum = 0;
+    let average = 0;
+    if (isEmpty(array)) return 0;
+    else {
+        array.map(function(num) {
+            sum = sum + num;
+        });
+        average = sum / array.length;
+    }
+    return average;
 }
